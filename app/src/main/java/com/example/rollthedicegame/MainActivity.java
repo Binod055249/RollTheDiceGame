d8370a2cdca722e10fb1aa233cbe7119923d1808
Binod Kumar <vk055249@gmail.com>
2020-08-09 19:34:48 +0530

    Roll the dice with secure random number generator

---
+++ app/src/main/java/com/example/rollthedicegame/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.rollthedicegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+
+public class MainActivity extends AppCompatActivity {
+
+    SecureRandom secureRandomNumber=new SecureRandom();
+    int number1=0;
+    int number2=0;
+    int number3=0;
+    int number4=0;
+    int number5=0;
+    int number6=0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView txtNumber1=findViewById(R.id.txtNumber1);
+        final TextView txtNumber2=findViewById(R.id.txtNumber2);
+        final TextView txtNumber3=findViewById(R.id.txtNumber3);
+        final TextView txtNumber4=findViewById(R.id.txtNumber4);
+        final TextView txtNumber5=findViewById(R.id.txtNumber5);
+        final TextView txtNumber6=findViewById(R.id.txtNumber6);
+
+        final ImageView imgDiceNumber1=findViewById(R.id.imgDiceNumber1);
+        final ImageView imgDiceNumber2=findViewById(R.id.imgDiceNumber2);
+        final ImageView imgDiceNumber3=findViewById(R.id.imgDiceNumber3);
+        final ImageView imgDiceNumber4=findViewById(R.id.imgDiceNumber4);
+        final ImageView imgDiceNumber5=findViewById(R.id.imgDiceNumber5);
+        final ImageView imgDiceNumber6=findViewById(R.id.imgDiceNumber6);
+
+        ImageView imgDice=findViewById(R.id.imgDice);
+        Button btnRollTheDice=findViewById(R.id.btnRollTheDice);
+
+        btnRollTheDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                for(int rollTimes=0; rollTimes<=50000;rollTimes++){
+
+                    int faceNumbers=1+secureRandomNumber.nextInt(6);
+
+                   switch (faceNumbers){
+
+                       case 1:
+                           ++number1;
+                           break;
+                       case 2:
+                           ++number2;
+                           break;
+                       case 3:
+                           ++number3;
+                           break;
+                       case 4:
+                           ++number4;
+                           break;
+                       case 5:
+                           ++number5;
+                           break;
+                       case 6:
+                           ++number6;
+                           break;
+
+                   }
+                }
+
+                txtNumber1.setText(number1+"");
+                txtNumber2.setText(number2+"");
+                txtNumber3.setText(number3+"");
+                txtNumber4.setText(number4+"");
+                txtNumber5.setText(number5+"");
+                txtNumber6.setText(number6+"");
+
+                imgDiceNumber1.setImageResource(R.drawable.dice1);
+                imgDiceNumber2.setImageResource(R.drawable.dice2);
+                imgDiceNumber3.setImageResource(R.drawable.dice3);
+                imgDiceNumber4.setImageResource(R.drawable.dice4);
+                imgDiceNumber5.setImageResource(R.drawable.dice5);
+                imgDiceNumber6.setImageResource(R.drawable.dice6);
+            }
+        });
+    }
+}
